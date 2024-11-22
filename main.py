f8b5f934d088f6bd9a9ce8be7bba8366aaeef868
CrazyJ18 <jsteinke310@gmail.com>
2024-11-22 15:58:40 -0600

    mark Window data members as private

--- main.py
+++ main.py
@@ -21,27 +21,27 @@ class Line():
 
 class Window():
     def __init__(self, width, height):
-        self.root = Tk()
-        self.root.title("Maze Solver")
-        self.canvas = Canvas(self.root, width=width, height=height)
-        self.canvas.pack(fill=BOTH, expand=1)
-        self.running = False
-        self.root.protocol("WM_DELETE_WINDOW", self.close)
+        self._root = Tk()
+        self._root.title("Maze Solver")
+        self._canvas = Canvas(self._root, width=width, height=height)
+        self._canvas.pack(fill=BOTH, expand=1)
+        self._running = False
+        self._root.protocol("WM_DELETE_WINDOW", self.close)
     
     def redraw(self):
-        self.root.update_idletasks()
-        self.root.update()
+        self._root.update_idletasks()
+        self._root.update()
     
     def wait_for_close(self):
-        self.running = True
-        while self.running:
+        self._running = True
+        while self._running:
             self.redraw()
     
     def close(self):
-        self.running = False
+        self._running = False
     
     def draw_line(self, line: Line, fill_color):
-        line.draw(self.canvas, fill_color)
+        line.draw(self._canvas, fill_color)
 
 
 class Cell():
