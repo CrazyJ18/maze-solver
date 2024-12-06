ee58c95100f3413e6a974fed60f9f7e796fc9982
CrazyJ18 <jsteinke310@gmail.com>
2024-12-06 09:48:58 -0600

    refactor Cell to not need coordinates on init

--- cell.py
+++ cell.py
@@ -2,34 +2,30 @@ from graphics import Window, Line, Point
 
 
 class Cell():
-    def __init__(self, x1, x2, y1, y2, win: Window):
+    def __init__(self, win: Window):
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
-        self._x1 = x1
-        self._x2 = x2
-        self._y1 = y1
-        self._y2 = y2
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
         self._win = win
     
-    def draw(self):
+    def draw(self, x1, y1, x2, y2):
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
         if self.has_left_wall:
-            self._win.draw_line(
-                Line(Point(self._x1, self._y1), Point(self._x1, self._y2))
-                )
+            self._win.draw_line(Line(Point(x1, y1), Point(x1, y2)))
         if self.has_top_wall:
-            self._win.draw_line(
-                Line(Point(self._x1, self._y1), Point(self._x2, self._y1))
-                )
+            self._win.draw_line(Line(Point(x1, y1), Point(x2, y1)))
         if self.has_right_wall:
-            self._win.draw_line(
-                Line(Point(self._x2, self._y1), Point(self._x2, self._y2))
-                )
+            self._win.draw_line(Line(Point(x2, y1), Point(x2, y2)))
         if self.has_bottom_wall:
-            self._win.draw_line(
-                Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
-                )
+            self._win.draw_line(Line(Point(x1, y2), Point(x2, y2)))
     
     def draw_move(self, to_cell, undo=False):
         start = self._centerpoint()
--- main.py
+++ main.py
@@ -4,20 +4,22 @@ from cell import Cell
 
 def main():
     win = Window(800, 600)
-    cells: list[Cell] = []
-    cells.append(Cell(200, 240, 200, 240, win))
-    cells.append(Cell(240, 280, 200, 240, win))
-    cells.append(Cell(240, 280, 240, 280, win))
+    cells = [Cell(win) for i in range(3)]
+    
     cells[0].has_right_wall = False
     cells[1].has_left_wall = False
     cells[1].has_bottom_wall = False
     cells[2].has_top_wall = False
-    for cell in cells:
-        cell.draw()
+    
+    cells[0].draw(200, 200, 240, 240)
+    cells[1].draw(240, 200, 280, 240)
+    cells[2].draw(240, 240, 280, 280)
+    
     cells[0].draw_move(cells[1])
     cells[1].draw_move(cells[2])
     cells[2].draw_move(cells[1], True)
     cells[1].draw_move(cells[0], True)
+    
     win.wait_for_close()
 
 
