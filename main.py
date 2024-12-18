c90af485df9933d41cd0789c2096126d3ca8187a
CrazyJ18 <jsteinke310@gmail.com>
2024-12-18 12:56:54 -0600

    generate maze

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from maze import Maze
 def main():
     win = Window(800, 600)
     
-    m = Maze(5, 5, 10, 10, 40, 40, win)
+    m = Maze(5, 5, 10, 10, 40, 40, win, 0)
     m._break_entrance_and_exit()
+    m._break_walls_r(0, 0)
     
     win.wait_for_close()
 
